0d531bf5194b40d1c225238ec992322abd25ada0
Imepnja <imepnja@gmail.com>
2020-06-27 13:26:18 +0200

    size fixed; add Item on submitted

--- lib/pages/add_item_page.dart
+++ lib/pages/add_item_page.dart
@@ -18,9 +18,19 @@ class _AddItemPageState extends State<AddItemPage> {
   String title = '';
   String description = '';
 
+  void addItem() {
+          if (_formKey.currentState.validate()) {
+                        Nav.pop(context);
+                        // if(widget.listKey.currentState != null) {
+                        // widget.listKey.currentState.insertItem(0);
+                        // }
+                        DatabaseService().addItem(
+                            title: title, description: description ?? '');
+                      }
+        }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
         appBar: AppBar(
           title: Text('Add Item', style: TextStyle()),
@@ -29,76 +39,69 @@ class _AddItemPageState extends State<AddItemPage> {
         body: Form(
           key: _formKey,
           child: Container(
-            height: double.infinity,
+            // height: double.infinity,
+            // width: double.infinity,
             padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-            child: IntrinsicHeight(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  // Container(color: Colors.cyanAccent, height: 100,),
-                  // Container(color: Colors.yellow),
-                  TextFormField(
-                    autofocus: true,
-                    validator: (val) => val.trim() == '' ? 'Add a Title' : null,
-                    onChanged: (val) => setState(() => title = val),
-                    decoration: InputDecoration(
-                      hintText: 'Title',
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide:
-                              BorderSide(color: Colors.white, width: 2.0)),
-                    ),
+            child: ListView(
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              // mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                // Container(color: Colors.cyanAccent, height: 100,),
+                // Container(color: Colors.yellow),
+                TextFormField(
+                  autofocus: true,
+                  validator: (val) => val.trim() == '' ? 'Add a Title' : null,
+                  onChanged: (val) => setState(() => title = val),
+                  onFieldSubmitted: (val) => addItem(),
+                  decoration: InputDecoration(
+                    hintText: 'Title',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide:
+                            BorderSide(color: Colors.white, width: 2.0)),
                   ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    onChanged: (val) => setState(() => description = val),
-                    decoration: InputDecoration(
-                      hintText: 'Description (optional)',
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide:
-                              BorderSide(color: Colors.white, width: 2.0)),
-                    ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  onChanged: (val) => setState(() => description = val),
+                  decoration: InputDecoration(
+                    hintText: 'Description (optional)',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide:
+                            BorderSide(color: Colors.white, width: 2.0)),
                   ),
-                  SizedBox(height: 20),
-                  Container(
-                    
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              blurRadius: 10,
-                              spreadRadius: 5,
-                              color: Colors.black12.withOpacity(0.04))
-                        ],
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(25)),
-                    width: double.infinity,
-                    child: FlatButton(
-                      child: Text('Add Item',
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
-                      onPressed: () {
-                        if(_formKey.currentState.validate()) {
-                          Nav.pop(context);
-                          // if(widget.listKey.currentState != null) {
-                          // widget.listKey.currentState.insertItem(0);
-                          // }
-                          DatabaseService().addItem(title: title, description: description ?? '');
-                        }
-                      },
-                    ),
-                  )
-                ],
-              ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10,
+                            spreadRadius: 5,
+                            color: Colors.black12.withOpacity(0.04))
+                      ],
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(25)),
+                  width: double.infinity,
+                  child: FlatButton(
+                    child: Text('Add Item',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
+                    onPressed: () => addItem(),
+                  ),
+                )
+              ],
             ),
           ),
         ));
+
+        
   }
 }
--- lib/pages/edit_item_page.dart
+++ lib/pages/edit_item_page.dart
@@ -35,105 +35,103 @@ class _EditItemPageState extends State<EditItemPage> {
           child: Container(
             height: double.infinity,
             padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-            child: IntrinsicHeight(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // Container(color: Colors.cyanAccent, height: 100,),
-                  // Container(color: Colors.yellow),
-                  TextFormField(
-                    initialValue: title,
-                    autofocus: true,
-                    validator: (val) => val.trim() == '' ? 'Add a Title' : null,
-                    onChanged: (val) => setState(() {
-                      edited = true;
-                      title = val;
-                    }),
-                    decoration: InputDecoration(
-                      hintText: 'Title',
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide:
-                              BorderSide(color: Colors.white, width: 2.0)),
-                    ),
+            child: ListView(
+              // crossAxisAlignment: CrossAxisAlignment.stretch,
+              // mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Container(color: Colors.cyanAccent, height: 100,),
+                // Container(color: Colors.yellow),
+                TextFormField(
+                  initialValue: title,
+                  autofocus: true,
+                  validator: (val) => val.trim() == '' ? 'Add a Title' : null,
+                  onChanged: (val) => setState(() {
+                    edited = true;
+                    title = val;
+                  }),
+                  decoration: InputDecoration(
+                    hintText: 'Title',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide:
+              BorderSide(color: Colors.white, width: 2.0)),
                   ),
-                  SizedBox(height: 20),
-                  TextFormField(
-                    initialValue: description,
-                    onChanged: (val) => setState(() {
-                      edited = true;
-                      description = val;
-                    }),
-                    decoration: InputDecoration(
-                      hintText: 'Description (optional)',
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25),
-                          borderSide:
-                              BorderSide(color: Colors.white, width: 2.0)),
-                    ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  initialValue: description,
+                  onChanged: (val) => setState(() {
+                    edited = true;
+                    description = val;
+                  }),
+                  decoration: InputDecoration(
+                    hintText: 'Description (optional)',
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+              borderSide:
+              BorderSide(color: Colors.white, width: 2.0)),
                   ),
-                  SizedBox(height: 20),
-                  Container(
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              blurRadius: 10,
-                              spreadRadius: 5,
-                              color: Colors.black12.withOpacity(0.04))
-                        ],
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(25)),
-                    width: double.infinity,
-                    child: FlatButton(
-                      child: Text('Save',
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
-                      onPressed: () {
-                        DatabaseService().editItem(
-                            title: title,
-                            description: description,
-                            id: widget.item.id);
-                        Nav.pop(context);
-                      },
-                    ),
+                ),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+              blurRadius: 10,
+              spreadRadius: 5,
+              color: Colors.black12.withOpacity(0.04))
+            ],
+            color: Theme.of(context).primaryColor,
+            borderRadius: BorderRadius.circular(25)),
+                  width: double.infinity,
+                  child: FlatButton(
+                    child: Text('Save',
+              style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white)),
+                    onPressed: () {
+            DatabaseService().editItem(
+            title: title,
+            description: description,
+            id: widget.item.id);
+            Nav.pop(context);
+                    },
                   ),
-                  SizedBox(height: 20),
-                  Container(
-                    decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              blurRadius: 10,
-                              spreadRadius: 5,
-                              color: Colors.black12.withOpacity(0.04))
-                        ],
-                        color: Colors.red,
-                        borderRadius: BorderRadius.circular(25)),
-                    width: double.infinity,
-                    child: FlatButton(
-                      child: Text('Delete Item',
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
-                      onPressed: () {
-                        // if(widget.listKey.currentState != null){
-                          // widget.listKey.currentState.removeItem(widget.item.currentPos, (context, animation) => SizedBox());
-                        // }
+                ),
+                SizedBox(height: 20),
+                Container(
+                  decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+              blurRadius: 10,
+              spreadRadius: 5,
+              color: Colors.black12.withOpacity(0.04))
+            ],
+            color: Colors.red,
+            borderRadius: BorderRadius.circular(25)),
+                  width: double.infinity,
+                  child: FlatButton(
+                    child: Text('Delete Item',
+              style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+              color: Colors.white)),
+                    onPressed: () {
+            // if(widget.listKey.currentState != null){
+              // widget.listKey.currentState.removeItem(widget.item.currentPos, (context, animation) => SizedBox());
+            // }
 
-                        DatabaseService().removeItem(widget.item.id);
-                        Nav.pop(context);
-                      },
-                    ),
-                  )
-                ],
-              ),
+            DatabaseService().removeItem(widget.item.id);
+            Nav.pop(context);
+                    },
+                  ),
+                )
+              ],
             ),
           ),
         ));
